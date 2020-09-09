15ff6d9a10dc25f7d6e1cc7038dfb15757973df7
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-09-09 14:20:14 +1200

    Basic timer

--- Timer.hpp
+++ Timer.hpp
@@ -1,7 +1,8 @@
 #ifndef Tools_Timer_hpp
 #define Tools_Timer_hpp
 
-#include <chrono> 
+#include <chrono>
+#include <iostream>
 
 using namespace std::chrono; 
   
@@ -12,27 +13,27 @@ class Timer
 {
     private:
         time_point<steady_clock> t0;
+        double seconds;
 
     public:
+
+        // Timer starts upon construction
         Timer();
-        void reset();
-        double elapsed_seconds();
+
+        // Stops the timer and returns the number of seconds
+        double stop();
 };
 
 /* IMPLEMENTATIONS FOLLOW */
 
 Timer::Timer()
 :t0(steady_clock::now())
+,seconds(0.0)
 {
 
 }
 
-void Timer::reset()
-{
-    t0 = steady_clock::now();
-}
-
-double Timer::elapsed_seconds()
+double Timer::stop()
 {
     return duration<double>(steady_clock::now() - t0).count();
 }
